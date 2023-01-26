904362a7317433ee987e5c9d403f02d3f946514f
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-26 10:40:31 +0100

    Add toggle functionality to mobilemenu

--- src/mobilemenu.js
+++ src/mobilemenu.js
@@ -3,13 +3,18 @@ import './mobilemenu.css';
 export default class MobileMenu {
     /**
      * 
-     * @param {string} position - Determines position of menu
+     * 
      */
-    constructor(position = 'top-left') {
+    constructor() {
         this.menuButton = document.createElement('button');
-        this.menuButton.classList.add('menu-button');
+        this.menuButton.classList.add('menu-button', 'menu-button-light');
+        this.menuButton.addEventListener('click', (e) => this.toggle());
 
         this.menuContainer = document.createElement('div');
         this.menuContainer.classList.add('menu-container');
+    }
+
+    toggle() {
+        this.menuContainer.classList.toggle('visible');
     }
 }
